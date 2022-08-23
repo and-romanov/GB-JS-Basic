b0e2d3adcb165e8d8683201e93e6d2d535d8e3a4
AndRomanov <beliypes@gmail.com>
2022-08-23 14:16:24 +0300

    В задаче 6 оператор switch не срабатывает.

---
+++ problem_5.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a + b;
+};
+
+function differrence(a, b) {
+    return a - b;
+};
+
+function multiplication(a, b) {
+    return a * b;
+};
+
+function division(a, b) {
+    return a / b;
+};
+
+console.log(sum(3, 5));
+console.log(differrence(3, 5));
+console.log(multiplication(3, 5));
+console.log(division(3, 5));
